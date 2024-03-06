d3d01b96e9f1b5ae2fbc1241859f3ad90f657011
cLausx0 <david.almeida2707@gmail.com>
2024-03-06 13:51:53 -0300

    Iniciando Observer Pattern

---
+++ Observer/CurrentConditionalDisplay.java
@@ -0,0 +1,20 @@
+public class CurrentConditionalDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionalDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
---
+++ Observer/WeatherStation.java
@@ -0,0 +1,23 @@
+public class WeatherStation {
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionalDisplay currentDisplay = new CurrentConditionalDisplay(weatherData);
+
+        StatiscticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+
+        weatherData.setMeasurements(82, 70, 29.2f);
+
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
