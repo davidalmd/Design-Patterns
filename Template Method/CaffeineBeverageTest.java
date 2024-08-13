68a52179e372986461e4682d6e7f997ce4dc91b4
cLausx0 <david.almeida2707@gmail.com>
2024-08-13 10:26:58 -0300

    Enviando padrão de projeto Template Method

---
+++ Template Method/CaffeineBeverageTest.java
@@ -0,0 +1,8 @@
+public class CaffeineBeverageTest {
+    public static void main(String[] args) {
+        Coffee coffee = new Coffee();
+
+        System.out.println("\nMaking coffee...");
+        coffee.prepareRecipe();
+    }
+}
